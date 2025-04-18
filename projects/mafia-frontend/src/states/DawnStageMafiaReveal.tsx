e6851a782d219d2a8bba4f7c50b6f2a074e3c162
ericsharma <sharma.e@husky.neu.edu>
2025-04-18 10:50:14 -0400

    refactor(components): extract player state logic to custom hook

--- projects/mafia-frontend/src/states/DawnStageMafiaReveal.tsx
+++ projects/mafia-frontend/src/states/DawnStageMafiaReveal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import usePlayersState from '../hooks/usePlayerState'
 import { Player } from '../interfaces/player'
 
 interface DawnStageMafiaRevealProps {
@@ -6,44 +6,7 @@ interface DawnStageMafiaRevealProps {
 }
 
 const DawnStageMafiaReveal: React.FC<DawnStageMafiaRevealProps> = ({ playerObject }) => {
-  const [iAmMafia, setIAmMafia] = useState<boolean>(false)
-  const intervalRef = useRef<NodeJS.Timeout | null>(null)
-
-  const fetchPlayers = async () => {
-    try {
-      const fetchedPlayers = [
-        { label: 'Player 1', address: (await playerObject.day_client.state.global.player1AlgoAddr())!.toString(), number: 1 },
-        { label: 'Player 2', address: (await playerObject.day_client.state.global.player2AlgoAddr())!.toString(), number: 2 },
-        { label: 'Player 3', address: (await playerObject.day_client.state.global.player3AlgoAddr())!.toString(), number: 3 },
-        { label: 'Player 4', address: (await playerObject.day_client.state.global.player4AlgoAddr())!.toString(), number: 4 },
-        { label: 'Player 5', address: (await playerObject.day_client.state.global.player5AlgoAddr())!.toString(), number: 5 },
-        { label: 'Player 6', address: (await playerObject.day_client.state.global.player6AlgoAddr())!.toString(), number: 6 },
-      ]
-
-      setIAmMafia(playerObject.night_algo_address.addr.toString() === (await playerObject.night_client.state.global.mafia())!.toString())
-    } catch (error) {
-      console.error('Failed to fetch players:', error)
-    }
-  }
-
-  const startPolling = () => {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current)
-    }
-    intervalRef.current = setInterval(() => {}, 2800) // Poll every 2.8 seconds
-  }
-
-  useEffect(() => {
-    // Run fetchPlayers initially when the component is loaded
-    fetchPlayers()
-    startPolling()
-    return () => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current) // Cleanup interval on component unmount
-      }
-    }
-  }, [])
-
+  const { iAmMafia } = usePlayersState(playerObject)
   const handleMafiaReveal = async () => {
     await playerObject.night_client.send.dawnStageMafiaReveal({
       args: {
